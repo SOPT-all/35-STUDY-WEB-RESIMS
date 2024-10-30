51f322c5426cb3062dfe76e09b987041ddca38be
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-10-31 00:24:33 +0900

    week1 실습 완료

--- week1/src/App.jsx
+++ week1/src/App.jsx
@@ -1,8 +1,23 @@
-// src/App.js
-import React from "react";
+import { ElementsProvider } from "./ElementContext";
+import Canvas from "./Canvas";
+import Sidebar from "./Sidebar";
+import PropertyPanel from "./PropertyPanel";
+import { AppContainer } from "./StyledComponents";
+import { SelectionProvider } from "./contexts/SelectionContext";
 
 function App() {
-  return <>1주차 실습</>;
+  console.log("App 렌더링");
+  return (
+    <ElementsProvider>
+      <SelectionProvider>
+        <AppContainer>
+          <Sidebar />
+          <Canvas />
+          <PropertyPanel />
+        </AppContainer>
+      </SelectionProvider>
+    </ElementsProvider>
+  );
 }
 
 export default App;
---
+++ week1/src/Sidebar.jsx
@@ -0,0 +1,34 @@
+// src/components/Sidebar.js
+import React, { useContext } from "react";
+import { ElementsContext } from "./ElementContext";
+import { SelectionContext } from "./contexts/SelectionContext";
+import { SidebarContainer } from "./StyledComponents";
+
+const Sidebar = () => {
+  const { elements } = useContext(ElementsContext);
+  const { selectedElementId, selectElement } = useContext(SelectionContext);
+
+  console.log("Sidebar 렌더링");
+
+  return (
+    <SidebarContainer>
+      <h3>Elements</h3>
+      <ul>
+        {elements.map((element) => (
+          <li
+            key={element.id}
+            onClick={() => selectElement(element.id)}
+            style={{
+              cursor: "pointer",
+              fontWeight: selectedElementId === element.id ? "bold" : "normal",
+            }}
+          >
+            {element.name}
+          </li>
+        ))}
+      </ul>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
