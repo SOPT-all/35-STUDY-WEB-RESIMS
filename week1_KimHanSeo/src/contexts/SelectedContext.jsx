125f8336a6f003c152c5bad9a5d7b8f6762d0e29
hanseo <yds06206@gmail.com>
2024-10-31 00:25:01 +0900

    week1 실습

---
+++ week1_KimHanSeo/src/contexts/SelectedContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+export const SelectionContext = createContext();
+
+export const SelectionProvider = ({ children }) => {
+  const [selectedElementId, setSelectedElementId] = useState(null);
+
+  const selectElement = (id) => {
+    setSelectedElementId(id);
+  };
+
+  return (
+    <SelectionContext.Provider value={{ selectedElementId, selectElement }}>
+      {children}
+    </SelectionContext.Provider>
+  );
+};
